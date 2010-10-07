5bfe4b54c5c3b89a682aa137998999af1a5c8f83
Sebastian Schaetz <seb.schaetz@gmail.com>
2010-10-07 16:36:13 +0200

    added window class that extends boost mpi with window and get/put functionality

---
+++ window.cpp
@@ -0,0 +1,203 @@
+#include <exception>
+#include <boost/throw_exception.hpp>
+#include "window.hpp"
+
+namespace boost {
+  namespace mpi {
+
+// _____________________________________________________________________________
+
+window::window(communicator & comm, bool no_locks)
+{
+  create_window_impl(NULL, 0, 1, comm, no_locks);
+}
+
+// _____________________________________________________________________________
+
+window::window(const void * base, int size, int disp_unit,
+               communicator & comm, bool no_locks)
+{
+  create_window_impl(base, size, disp_unit, comm, no_locks);
+}
+
+// _____________________________________________________________________________
+
+void window::create_window_impl(const void * base, int size, int disp_unit,
+                                communicator & comm, bool no_locks)
+{
+  MPI_Info info;
+  BOOST_MPI_CHECK_RESULT(MPI_Info_create, (&info));
+  if(no_locks)
+  {
+    BOOST_MPI_CHECK_RESULT(MPI_Info_set, (info, (char *)"no_locks",
+                                          (char *)"true"));
+  }
+  else
+  {
+    BOOST_MPI_CHECK_RESULT(MPI_Info_set, (info, (char *)"no_locks",
+                                          (char *)"false"));
+  }
+
+  BOOST_MPI_CHECK_RESULT(MPI_Win_create, (const_cast<void *>(base), size,
+    disp_unit, info, MPI_Comm(comm), &win));
+}
+
+// _____________________________________________________________________________
+
+int window::size() const
+{
+  unsigned int size_ = 0;
+  void * sp = 0;
+  int flag;
+  BOOST_MPI_CHECK_RESULT(MPI_Win_get_attr, (win, MPI_WIN_SIZE,
+                                            (void*)&sp, &flag));
+  if(flag)
+  {
+    size_ = *(int*)sp;
+  }
+  return size_;
+}
+
+// _____________________________________________________________________________
+
+int window::dispunit() const
+{
+  unsigned int dispunit_ = 0;
+  void * dp = 0;
+  int flag;
+  BOOST_MPI_CHECK_RESULT(MPI_Win_get_attr,
+                         (win, MPI_WIN_DISP_UNIT, (void*)&dp, &flag));
+  if(flag)
+  {
+    dispunit_ = *(int*)dp;
+  }
+  return dispunit_;
+}
+
+// _____________________________________________________________________________
+
+void window::put_impl(const void * origin_addr, int origin_count,
+                      MPI_Datatype origin_datatype, int target_rank,
+                      int target_disp, int target_count,
+                      MPI_Datatype target_datatype, mpl::true_) const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Put, (const_cast<void *>(origin_addr),
+                                   origin_count, origin_datatype,
+                                   target_rank, target_disp,
+                                   target_count, target_datatype,
+                                   win));
+}
+
+// _____________________________________________________________________________
+
+void window::put_impl(const void * origin_addr, int origin_count,
+                      MPI_Datatype origin_datatype, int target_rank,
+                      int target_disp, int target_count,
+                      MPI_Datatype target_datatype, mpl::false_) const
+{
+  boost::throw_exception(boost::mpi::exception(
+    "one sided communication currently supports only MPI datatypes", 0));
+}
+
+// _____________________________________________________________________________
+
+void window::get_impl(const void * origin_addr, int origin_count,
+                      MPI_Datatype origin_datatype, int target_rank,
+                      int target_disp, int target_count,
+                      MPI_Datatype target_datatype, mpl::true_) const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Get, (const_cast<void *>(origin_addr),
+                                   origin_count, origin_datatype,
+                                   target_rank, target_disp,
+                                   target_count, target_datatype,
+                                   win));
+}
+
+// _____________________________________________________________________________
+
+void window::get_impl(const void * origin_addr, int origin_count,
+                      MPI_Datatype origin_datatype, int target_rank,
+                      int target_disp, int target_count,
+                      MPI_Datatype target_datatype, mpl::false_) const
+{
+  boost::throw_exception(boost::mpi::exception(
+    "one sided communication currently supports only MPI datatypes", 0));
+}
+
+// _____________________________________________________________________________
+
+void window::fence() const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_fence, (0, win));
+}
+
+// _____________________________________________________________________________
+
+void window::start(group g) const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_start, (MPI_Group(g), 0, win));
+}
+
+// _____________________________________________________________________________
+
+void window::complete() const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_complete, (win));
+}
+
+// _____________________________________________________________________________
+
+void window::post(group g) const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_post, (MPI_Group(g), 0, win));
+}
+
+// _____________________________________________________________________________
+
+void window::wait() const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_wait, (win));
+}
+
+// _____________________________________________________________________________
+
+bool window::test() const
+{
+  int flag;
+  BOOST_MPI_CHECK_RESULT(MPI_Win_test, (win, &flag));
+  if(flag)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+// _____________________________________________________________________________
+
+void window::lock(int lock_type, int rank) const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_lock, (lock_type, rank, 0, win));
+}
+
+// _____________________________________________________________________________
+
+void window::unlock(int rank) const
+{
+  BOOST_MPI_CHECK_RESULT(MPI_Win_unlock, (rank, win));
+}
+
+// _____________________________________________________________________________
+
+window::~window()
+{
+  MPI_Win_free(&win);
+}
+
+// _____________________________________________________________________________
+
+  }
+}
+
---
+++ window.hpp
@@ -0,0 +1,248 @@
+#ifndef BOOST_MPI_WINDOW_HPP
+#define BOOST_MPI_WINDOW_HPP
+
+#include <boost/mpi/config.hpp>
+#include <boost/mpi/exception.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <boost/mpi/group.hpp>
+
+namespace boost {
+  namespace mpi {
+
+/**
+ * @brief A constant representing an exclusive lock
+ */
+const int lock_exclusive = MPI_LOCK_EXCLUSIVE;
+
+/**
+ * @brief A constant representing a shared lock
+ */
+const int lock_shared = MPI_LOCK_SHARED;
+
+
+class BOOST_MPI_DECL window
+{
+
+ public:
+
+  /**
+   * Create an empty window
+   */
+  window(communicator & comm, bool no_locks = false);
+
+  /**
+   * Create window on one value
+   */
+  template<typename T>
+  window(const T & base, communicator & comm, bool no_locks = false);
+
+  /**
+   * Create window on array of values
+   */
+  template<typename T>
+  window(const T * base, int n, communicator & comm, bool no_locks = false);
+
+  /**
+   * Create window on block of memory
+   */
+  window(const void * base, int size, int disp_unit,
+         communicator & comm, bool no_locks = false);
+
+  /**
+   * Get the base address of the window
+   */
+  template<typename T>
+  T * base() const;
+
+  /**
+   * Get the size of the window
+   */
+  int size() const;
+
+  /**
+   * Get the displacement unit of the window
+   */
+  int dispunit() const;
+
+  /**
+   * Put one value to a destination
+   * Data is copied from the origin to the target
+   */
+  template<typename T>
+  void put(const T & origin, int target_rank, int target_disp) const;
+
+  /**
+   * Put an array of values to a destination
+   * Data is copied from the origin to the target
+   */
+  template<typename T>
+  void put(const T * origin, int count, int target_rank, int target_disp) const;
+
+  /**
+   * Get one value from a destination
+   * Data is copied from the target to the origin
+   */
+  template<typename T>
+  void get(const T & origin, int target_rank, int target_disp) const;
+
+  /**
+   * Get an array of values from a destination
+   * Data is copied from the target to the origin
+   */
+  template<typename T>
+  void get(const T * origin, int count, int target_rank, int target_disp) const;
+
+  /**
+   * Synchornize remote memory access calls
+   */
+  void fence() const;
+
+  /**
+   * Start remote memory access epoch
+   */
+  void start(group g) const;
+
+  /**
+   * Complete remote memory access epoch
+   */
+  void complete() const;
+
+  /**
+   * Start remote memory access exposure epoch
+   */
+  void post(group g) const;
+
+  /**
+   * Complete remote memory access exposure epoch
+   */
+  void wait() const;
+
+  /**
+   * Test if remote memory access exposure epoch is completed
+   */
+  bool test() const;
+
+  /**
+   * Start remote memory access epoch
+   */
+  void lock(int lock_type, int rank) const;
+
+  /**
+   * Complete remote memory access epoch
+   */
+  void unlock(int rank) const;
+
+  /**
+   * Destory window
+   */
+  ~window();
+
+ private:
+
+  void create_window_impl(const void * base, int size, int disp_unit,
+                          communicator & comm, bool no_locks = false);
+
+  void put_impl(const void * origin_addr, int origin_count,
+                MPI_Datatype origin_datatype, int target_rank, int target_disp,
+                int target_count, MPI_Datatype target_datatype,
+                mpl::true_) const;
+
+  void put_impl(const void * origin_addr, int origin_count,
+                MPI_Datatype origin_datatype, int target_rank, int target_disp,
+                int target_count, MPI_Datatype target_datatype,
+                mpl::false_) const;
+
+  void get_impl(const void * origin_addr, int origin_count,
+                MPI_Datatype origin_datatype, int target_rank, int target_disp,
+                int target_count, MPI_Datatype target_datatype,
+                mpl::true_) const;
+
+
+  void get_impl(const void * origin_addr, int origin_count,
+                MPI_Datatype origin_datatype, int target_rank, int target_disp,
+                int target_count, MPI_Datatype target_datatype,
+                mpl::false_) const;
+
+  MPI_Win win;
+
+};
+
+
+// Implementation
+// _____________________________________________________________________________
+
+
+template<typename T>
+window::window(const T& base, communicator & comm, bool no_locks)
+{
+  create_window_impl((void *) &base, sizeof(T), sizeof(T), comm, no_locks);
+}
+
+// _____________________________________________________________________________
+
+template<typename T>
+window::window(const T * base, int n, communicator & comm, bool no_locks)
+{
+  create_window_impl((void *) base, sizeof(T)*n, sizeof(T), comm, no_locks);
+}
+
+// _____________________________________________________________________________
+
+template<typename T>
+T * window::base() const
+{
+  T * base;
+  int flag;
+  BOOST_MPI_CHECK_RESULT(MPI_Win_get_attr, (win, MPI_WIN_BASE, &base, &flag));
+  return base;
+}
+
+// _____________________________________________________________________________
+
+template<typename T>
+void window::put(const T & origin, int target_rank, int target_disp) const
+{
+  put_impl((void *)&origin, 1, get_mpi_datatype<T>(origin), target_rank,
+           target_disp, 1, get_mpi_datatype<T>(*origin),
+           is_mpi_datatype<T>());
+}
+
+// _____________________________________________________________________________
+
+template<typename T>
+void window::put(const T * origin, int count,
+                 int target_rank, int target_disp) const
+{
+  put_impl((void *)origin, count, get_mpi_datatype<T>(*origin), target_rank,
+           target_disp, count, get_mpi_datatype<T>(*origin),
+           is_mpi_datatype<T>());
+}
+
+// _____________________________________________________________________________
+
+template<typename T>
+void window::get(const T & origin,
+                 int target_rank, int target_disp) const
+{
+  get_impl((void *)&origin, 1, get_mpi_datatype<T>(origin), target_rank,
+           target_disp, 1, get_mpi_datatype<T>(*origin),
+           is_mpi_datatype<T>());
+}
+
+// _____________________________________________________________________________
+
+template<typename T>
+void window::get(const T * origin, int count,
+                 int target_rank, int target_disp) const
+{
+  get_impl((void *)origin, count, get_mpi_datatype<T>(*origin), target_rank,
+           target_disp, count, get_mpi_datatype<T>(*origin),
+           is_mpi_datatype<T>());
+}
+
+// _____________________________________________________________________________
+
+  }
+}
+
+#endif // BOOST_MPI_WINDOW_HPP
