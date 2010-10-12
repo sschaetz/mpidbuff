1138096549ea5bd666dc245a1718a1af27e9f6f5
Sebastian Schaetz <seb.schaetz@gmail.com>
2010-10-12 11:32:41 +0200

    Changed fusion serialization template to work with MPI

--- test/fusion.cpp
+++ test/fusion.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <fstream>
 #include <memory>
+#include <boost/mpi.hpp>
 #include <boost/fusion/sequence.hpp>
 #include <boost/fusion/include/sequence.hpp>
 #include <boost/fusion/container/generation/make_vector.hpp>
@@ -18,8 +19,10 @@
 
 #include <ext/fusion/serialization/include.hpp>
 
+#include "lib/mpi_workaround.hpp"
 
 using namespace boost::fusion;
+namespace mpi = boost::mpi;
 
 struct print_xml
 {
@@ -58,25 +61,21 @@ class testclass
 int main(int argc, char* argv[])
 {
 
-  testclass<vector<int, char, std::string> >
-    w1("hi", make_vector(1, 'x', "howdy"));
-  // save data to archive
-  {
-    std::ofstream ofs("archive.bin");
-    boost::archive::text_oarchive oa(ofs);
-    oa << w1;
-    for_each(w1.t, print_xml());
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
 
+  if(world.rank() == 0) // test with wrapper
+  {
+    testclass<vector<int, char, std::string> >
+      w("hi", make_vector(1, 'x', "howdy"));
+    for_each(w.t, print_xml());
+    mpi_send_workaround(1, 0, w, world);
   }
-
-  testclass<vector<int, char, std::string> > w2;
-  // load data from archive
+  else if(world.rank() == 1)
   {
-    std::ifstream ifs("archive.bin");
-    boost::archive::text_iarchive ia(ifs);
-    ia >> w2;
-    for_each(w2.t, print_xml());
-
+    testclass<vector<int, char, std::string> > w;
+    mpi_recv_workaround(0, 0, w, world);
+    for_each(w.t, print_xml());
   }
 
 }
